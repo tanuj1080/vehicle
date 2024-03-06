51db5cfcd5886f702074dce26526eebafe8fd723
Animesh Bhawtankar <animesh7999@gmail.com>
2024-03-06 12:09:29 -0500

    Vehicle code

--- Vehicle.cs
+++ Vehicle.cs
@@ -1,11 +1,29 @@
-// Hello World! program
+// Hello 
 namespace Animesh-Vehicle
 {
 	class Vehicle
+{
+	private Engine engine;
+	private Wheel[] wheels;
+
+	public Vehicle()
 	{
-		static void Main(string[] args)
+		engine = new Engine();
+		wheels = new Wheel[4]; 
+		for (int i = 0; i < 4; i++)
 		{
-			System.Console.WriteLine("Hello World!");
+			wheels[i] = new Wheel();
 		}
 	}
+
+	public void Start()
+	{
+		engine.Start();
+		foreach (var wheel in wheels)
+		{
+			wheel.Rotate();
+		}
+		Console.WriteLine("The vehicle is starting.");
+	}
+
 }
